ba84b15aecb7c4d85a49e717fef7d924aaf96593
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-15 17:53:44 +0530

    init:project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Model } from './sections/Model'
+import { Navbar } from './sections/Navbar'
+const App = () => {
+  return (
+    <main className='max-w-7xl mx-auto'>
+      <Navbar />
+      <Model />
+    </main>
+  )
+}
+
+export default App
