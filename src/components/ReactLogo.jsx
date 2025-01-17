b984b812f4b28d77c83d8d5dcab65814ee3fc5a9
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-18 00:57:44 +0530

    update:material

--- src/components/ReactLogo.jsx
+++ src/components/ReactLogo.jsx
@@ -1,14 +1,22 @@
+import { MeshStandardMaterial } from 'three';
 import { Float, useGLTF } from '@react-three/drei';
 
 const ReactLogo = (props) => {
-  const { nodes, materials } = useGLTF('models/react.glb');
+  const { nodes } = useGLTF('models/react.glb');
+
+  const reactMaterial = new MeshStandardMaterial({
+    color: '#61dafb',
+    emissive: '#000000',
+    metalness: 0.5,
+    roughness: 0.3
+  })
 
   return (
     <Float floatIntensity={1}>
       <group position={[8, 8, 0]} {...props} dispose={null}>
         <mesh
           geometry={nodes['React-Logo_Material002_0'].geometry}
-          material={materials['Material.002']}
+          material={reactMaterial}
         />
       </group>
     </Float>
