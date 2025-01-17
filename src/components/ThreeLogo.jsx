070c75db590eaa0122b3e23e09359e3fe0d55804
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-18 00:58:53 +0530

    remove:animation update:material

--- src/components/ThreeLogo.jsx
+++ src/components/ThreeLogo.jsx
@@ -1,49 +1,49 @@
-import gsap from 'gsap';
-import { useRef, useState, useEffect } from 'react';
 import { Float, useGLTF } from '@react-three/drei';
-import * as THREE from 'three';
+import { MeshStandardMaterial } from 'three';
 
 const ThreeLogo = ({ ...props }) => {
   const { nodes } = useGLTF('/models/threeJs/scene.gltf');
-  const cubeRef = useRef();
-  const [hovered, setHovered] = useState(false);
-  const tlRef = useRef();
+  // const threeLogoRef = useRef();
+  // const [hovered, setHovered] = useState(false);
+  // const tlRef = useRef();
 
-  useEffect(() => {
-    tlRef.current = gsap.timeline({ repeat: -1, paused: true })
-      .to(cubeRef.current.rotation, {
-        y: `+=${Math.PI * 2}`,
-        x: `+=${Math.PI * 2}`,
-        duration: 5,
-        ease: 'none',
-      });
-    tlRef.current.play();
-  }, []);
+  // useEffect(() => {
+  //   tlRef.current = gsap.timeline({ repeat: -1, paused: true })
+  //     .to(threeLogoRef.current.rotation, {
+  //       y: `+=${Math.PI * 2}`,
+  //       x: `+=${Math.PI * 2}`,
+  //       duration: 5,
+  //       ease: 'none',
+  //     });
+  //   tlRef.current.play();
+  // }, []);
 
-  useEffect(() => {
-    if (hovered) {
-      tlRef.current.pause();
-    } else {
-      tlRef.current.resume();
-    }
-  }, [hovered]);
+  // useEffect(() => {
+  //   if (hovered) {
+  //     tlRef.current.pause();
+  //   } else {
+  //     tlRef.current.resume();
+  //   }
+  // }, [hovered]);
 
-  const goldMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffff00,
-    emissive: 0xffff00,
+  const goldMaterial = new MeshStandardMaterial({
+    color: '#ffd700',
+    emissive: '#000000',
+    metalness: 1,
+    roughness: 0.1,
   });
 
   return (
-    <Float floatIntensity={2}>
+    <Float floatIntensity={0.5}>
       <group rotation={[0, 0, 0]} dispose={null} {...props}>
         <mesh
-          ref={cubeRef}
+          // ref={threeLogoRef}
           castShadow
           receiveShadow
           geometry={nodes.Object_5.geometry}
           material={goldMaterial}
-          onPointerEnter={() => setHovered(true)}
-          onPointerLeave={() => setHovered(false)}
+          // onPointerEnter={() => setHovered(true)}
+          // onPointerLeave={() => setHovered(false)}
         />
       </group>
     </Float>
