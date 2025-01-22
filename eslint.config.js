b01ab0eaa95bc613996b502de698b8f90b309eb6
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-23 00:15:34 +0530

    add:tailwindPlugin add:indentRule

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import react from 'eslint-plugin-react'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import { ReactThreeFiber } from '@react-three/fiber'
+import js from '@eslint/js';
+import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { ReactThreeFiber } from '@react-three/fiber';
+import tailwind from 'eslint-plugin-tailwindcss';
 
 export default [
   { ignores: ['dist'] },
@@ -30,12 +31,17 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      ...tailwind.configs['flat/recommended'].rules,
       'react/jsx-no-target-blank': 'off',
       'react/no-unknown-property': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
+      // Add indentation rules
+      'indent': ['error', 2, { SwitchCase: 1, ignoredNodes: ['TemplateLiteral'] }],
+      'react/jsx-indent': ['error', 2],
+      'react/jsx-indent-props': ['error', 2],
     },
   },
-]
+];
---
+++ src/constants/index.js
@@ -0,0 +1,257 @@
+export const navLinks = [
+      {
+        id: 1,
+        name: 'Home',
+        href: '#home',
+      },
+      {
+        id: 2,
+        name: 'About',
+        href: '#about',
+      },
+      {
+        id: 3,
+        name: 'Work',
+        href: '#work',
+      },
+      {
+        id: 4,
+        name: 'Contact',
+        href: '#contact',
+      },
+];
+
+export const myProjects = [
+  {
+    title: 'Podcastr - AI Podcast Platform',
+    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    subdesc:
+      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
+    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+    texture: '/textures/project/project1.mp4',
+    logo: '/assets/project-logo1.png',
+    logoStyle: {
+      backgroundColor: '#2A1816',
+      border: '0.2px solid #36201D',
+      boxShadow: '0px 0px 60px 0px #AA3C304D',
+    },
+    spotlight: '/assets/spotlight1.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'LiveDoc - Real-Time Google Docs Clone',
+    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    subdesc:
+      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
+    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+    texture: '/textures/project/project2.mp4',
+    logo: '/assets/project-logo2.png',
+    logoStyle: {
+      backgroundColor: '#13202F',
+      border: '0.2px solid #17293E',
+      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+    },
+    spotlight: '/assets/spotlight2.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'CarePulse - Health Management System',
+    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    subdesc:
+      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
+    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+    texture: '/textures/project/project3.mp4',
+    logo: '/assets/project-logo3.png',
+    logoStyle: {
+      backgroundColor: '#60f5a1',
+      background:
+        'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
+      border: '0.2px solid rgba(208, 213, 221, 1)',
+      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+    },
+    spotlight: '/assets/spotlight3.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Horizon - Online Banking Platform',
+    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
+    subdesc:
+      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
+    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    texture: '/textures/project/project4.mp4',
+    logo: '/assets/project-logo4.png',
+    logoStyle: {
+      backgroundColor: '#0E1F38',
+      border: '0.2px solid #0E2D58',
+      boxShadow: '0px 0px 60px 0px #2F67B64D',
+    },
+    spotlight: '/assets/spotlight4.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+  {
+    title: 'Imaginify - AI Photo Manipulation App',
+    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
+    subdesc:
+      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
+    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
+    texture: '/textures/project/project5.mp4',
+    logo: '/assets/project-logo5.png',
+    logoStyle: {
+      backgroundColor: '#1C1A43',
+      border: '0.2px solid #252262',
+      boxShadow: '0px 0px 60px 0px #635BFF4D',
+    },
+    spotlight: '/assets/spotlight5.png',
+    tags: [
+      {
+        id: 1,
+        name: 'React.js',
+        path: '/assets/react.svg',
+      },
+      {
+        id: 2,
+        name: 'TailwindCSS',
+        path: 'assets/tailwindcss.png',
+      },
+      {
+        id: 3,
+        name: 'TypeScript',
+        path: '/assets/typescript.png',
+      },
+      {
+        id: 4,
+        name: 'Framer Motion',
+        path: '/assets/framer.png',
+      },
+    ],
+  },
+];
+
+export const calculateSizes = (isSmall, isMobile, isTablet) => {
+  console.log(isSmall, isMobile, isTablet);
+  return {
+    desktopScale: isSmall ? 1 : isMobile ? 1 : 1.4,
+    desktopPosition: isSmall ?  [7.3, 1.7, -2.5] : isTablet ? [10, -1, 1] : isMobile ? [7.3, 3.9, -2.5] : [9.7, -9, 11],
+    desktopRotation: isSmall ? [0.5, 0.8, 0] : isMobile ? [0.5, 0.8, 0] :  isTablet ? [0.5, 0.8, 0] : [0.3, 0.6, 0],
+
+    reactLogoScale: isSmall ? 0.2 : isMobile ? 0.3 : isTablet ? 0.3 : 0.7,
+    reactLogoPosition: isSmall ? [10, 10, 0] : isMobile ? [9, 12, 0] : isTablet ? [12, 12, 0] : [28, 5, 0],
+
+    threeLogoScale: isSmall ? 0.02 : isMobile ? 0.03 : isTablet ? 0.03 : 0.08,
+    threeLogoPosition: isSmall ? [-9, 11, 0] : isMobile ? [-9, 13.5, 0] : isTablet ? [-13, 14, 0] : [-32, 4.5, 0],
+    
+    astroScale: isSmall ? 0.7 : isMobile ? 0.7 : isTablet ? 0.7 : 1.5,
+    astroPosition: isSmall ? [-5, 7, 0] : isMobile ? [-10, 10, 0] : isTablet ? [-12, 10, 0] : [-24, 10, 0],
+
+    htmlCssLogoScale: 0.03,
+    htmlPosition: [-5.2, -11.2, 0],
+    htmlRotation: [0, 0, 0.2],
+
+    cssPosition: [6.2, -11.2, 0],
+    cssRotation: [0, 0, -0.2],
+
+    arrowScale: 20,
+    arrowPosition: [0, -13.6, 0],
+    arrowRotation: [0, -3.2, 0.1],
+  };
+};
+
+export const latLogWorkPlace = [
+  { lat: 11.0168, lng: 76.9558, size: 30, color: 'white' },
+  // { lat: 18.5204, lng: 73.8567, size: 15, color: 'red' },
+  // { lat: 13.0843, lng: 80.2705, size: 15, color: 'white' },
+  // { lat: 12.9716, lng: 77.5946, size: 15, color: 'red' },
+  // { lat: 17.4065, lng: 78.4772, size: 15, color: 'white' },
+]
+
+export const latLogWorkLabel = [
+  { lat: 11.0168, lng: 76.9558, size: 7, color: 'white', text: 'Coimbatore, India'},
+  // { lat: 18.5204, lng: 73.8567, size: 7, color: 'white', text: 'Pune'},
+  // { lat: 13.0843, lng: 80.2705, size: 4, color: 'white', text: 'Chennai'},
+  // { lat: 12.9716, lng: 77.5946, size: 4, color: 'white', text: 'Bangalore'},
+  // { lat: 17.4065, lng: 78.4772, size: 7, color: 'white', text: 'Hyderabad'},
+]
