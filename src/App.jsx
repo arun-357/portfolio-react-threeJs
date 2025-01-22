ae8de1349010a3d0728bc597bf8882c20ef1a497
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-22 16:23:28 +0530

    update:techStack added:lightWeightCarousel

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Model from './sections/Model'
 import Navbar from './sections/Navbar'
 import About from './sections/About'
 import TechStack from './sections/TechStack'
+// import Projects from './sections/Projects'
 export default function App() {
   return (
-    <main className='max-w-7xl mx-auto'>
+    <main className='max-w-8xl mx-auto'>
       <Navbar />
       <Model />
       <About />
       <TechStack />
+      {/* <Projects /> */}
     </main>
   )
 }
