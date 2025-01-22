19178a5824b733fcd67cc321d6a55e45cc037dd0
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-23 00:18:03 +0530

    add:constant

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,39 +1,39 @@
 export const navLinks = [
-      {
-        id: 1,
-        name: 'Home',
-        href: '#home',
-      },
-      {
-        id: 2,
-        name: 'About',
-        href: '#about',
-      },
-      {
-        id: 3,
-        name: 'Work',
-        href: '#work',
-      },
-      {
-        id: 4,
-        name: 'Contact',
-        href: '#contact',
-      },
+  {
+    id: 1,
+    name: 'Home',
+    href: '#home',
+  },
+  {
+    id: 2,
+    name: 'About',
+    href: '#about',
+  },
+  {
+    id: 3,
+    name: 'Work',
+    href: '#work',
+  },
+  {
+    id: 4,
+    name: 'Contact',
+    href: '#contact',
+  },
 ];
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: '3D Portfolio Website',
+    desc: 'This 3D portfolio website is an innovative platform that showcases projects and skills through an interactive and visually captivating experience.',
     subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+      'Built with advanced technologies like Three.js, React, and Tailwind CSS, this portfolio demonstrates technical expertise and creativity, delivering an immersive and engaging presentation for visitors.',    
+    href: 'https://github.com/arun-357/portfolio-react-threeJs',
     texture: '/textures/project/project1.mp4',
     logo: '/assets/project-logo1.png',
     logoStyle: {
       backgroundColor: '#2A1816',
       border: '0.2px solid #36201D',
-      boxShadow: '0px 0px 60px 0px #AA3C304D',
+      boxShadow: '0px 0px 60px 0px #ffffff',
     },
     spotlight: '/assets/spotlight1.png',
     tags: [
@@ -49,59 +49,59 @@ export const myProjects = [
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: 'Three Js',
+        path: '/assets/threeJs.png',
       },
     ],
   },
   {
-    title: 'LiveDoc - Real-Time Google Docs Clone',
-    desc: 'LiveDoc is a powerful collaborative app that elevates the capabilities of real-time document editing. As an enhanced version of Google Docs, It supports millions of collaborators simultaneously, ensuring that every change is captured instantly and accurately.',
+    title: 'Jira Time Log Reminder',
+    desc: 'As a Scrum Master, ensuring accurate time logging for generating effective burndown charts was a challenge. The Jira Time Log Reminder automates this process, saving time and improving team efficiency.',
     subdesc:
-      'With LiveDoc, users can experience the future of collaboration, where multiple contributors work together in real time without any lag, by using Next.js and Liveblocks newest features.',
-    href: 'https://www.youtube.com/watch?v=y5vE8y_f_OM',
+      'Built using Jira Open API, Google Script, Node.js, and Google Chat API, this tool sends automated reminders to team members at the end of each day, ensuring timely and accurate time logging.',    
+    href: 'https://github.com/arun-357/jiraTimeLogReminder',
     texture: '/textures/project/project2.mp4',
     logo: '/assets/project-logo2.png',
     logoStyle: {
       backgroundColor: '#13202F',
       border: '0.2px solid #17293E',
-      boxShadow: '0px 0px 60px 0px #2F6DB54D',
+      boxShadow: '0px 0px 60px 0px #ffffff',
     },
     spotlight: '/assets/spotlight2.png',
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: 'Jira Open API',
+        path: '/assets/jira.png',
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
+        name: 'Node.js',
+        path: 'assets/nodeJs.png',
       },
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
+        name: 'Google Scripts',
+        path: '/assets/google.png',
       },
       {
         id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
+        name: 'Google Chat',
+        path: 'assets/chat.png'
+      },
+      {
+        id: 5,
+        name: 'Open API',
+        path: '/assets/openAPI.png',
       },
     ],
   },
   {
-    title: 'CarePulse - Health Management System',
-    desc: 'An innovative healthcare platform designed to streamline essential medical processes. It simplifies patient registration, appointment scheduling, and medical record management, providing a seamless experience for both healthcare providers and patients.',
+    title: 'Voice Enhancer Telegram Bot',
+    desc: 'As an influencer, I needed a quick and efficient solution to enhance audio quality for my content. The Voice Enhancer Telegram Bot was designed to address this need, eliminating the hassle of using tedious online tools.',
     subdesc:
-      'With a focus on efficiency, CarePulse integrantes complex forms and SMS notifications, by using Next.js, Appwrite, Twillio and Sentry that enhance operational workflows.',
-    href: 'https://www.youtube.com/watch?v=lEflo_sc82g',
+      'Created using Python, Telegram Open API, and Spotify’s Pedalboard library, this bot simplifies voice enhancement, providing a fast and reliable tool tailored for content creators !',    
+    href: 'https://github.com/arun-357/voiceEnhancer-telegramBot',
     texture: '/textures/project/project3.mp4',
     logo: '/assets/project-logo3.png',
     logoStyle: {
@@ -109,106 +109,64 @@ export const myProjects = [
       background:
         'linear-gradient(0deg, #60F5A150, #60F5A150), linear-gradient(180deg, rgba(255, 255, 255, 0.9) 0%, rgba(208, 213, 221, 0.8) 100%)',
       border: '0.2px solid rgba(208, 213, 221, 1)',
-      boxShadow: '0px 0px 60px 0px rgba(35, 131, 96, 0.3)',
+      boxShadow: '0px 0px 60px 0px #ffffff',
     },
     spotlight: '/assets/spotlight3.png',
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: 'Python',
+        path: '/assets/python.png',
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
+        name: 'Telegram',
+        path: 'assets/tele.png',
+      }, 
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
+        name: 'Spotify Pedalboard',
+        path: 'assets/spotify.png',
+      }
     ],
   },
   {
-    title: 'Horizon - Online Banking Platform',
-    desc: 'Horizon is a comprehensive online banking platform that offers users a centralized finance management dashboard. It allows users to connect multiple bank accounts, monitor real-time transactions, and seamlessly transfer money to other users.',
-    subdesc:
-      'Built with Next.js 14 Appwrite, Dwolla and Plaid, Horizon ensures a smooth and secure banking experience, tailored to meet the needs of modern consumers.',
-    href: 'https://www.youtube.com/watch?v=PuOVqP_cjkE',
+    title: 'Jane Street Market Prediction',
+    desc: 'This project focuses on predicting market outcomes with high accuracy, leveraging advanced data analysis and machine learning techniques to model financial market behavior.',
+    subdesc:  
+      'Developed as part of a Kaggle competition, this solution achieved a top 3% ranking among thousands of participants. Built using Python and XBG Boost machine learning frameworks.',     
+    href: 'https://github.com/arun-357/janeStreet-marketPrediction',
     texture: '/textures/project/project4.mp4',
-    logo: '/assets/project-logo4.png',
+    logo: '/assets/jane.png',
     logoStyle: {
-      backgroundColor: '#0E1F38',
-      border: '0.2px solid #0E2D58',
-      boxShadow: '0px 0px 60px 0px #2F67B64D',
+      backgroundColor: '#ffffff',
+      border: '0.2px solidrgb(112, 135, 166)',
+      boxShadow: '0px 0px 60px 0px #ffffff',
     },
     spotlight: '/assets/spotlight4.png',
     tags: [
       {
         id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: 'Python',
+        path: '/assets/python.png',
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
-      {
-        id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
-    ],
-  },
-  {
-    title: 'Imaginify - AI Photo Manipulation App',
-    desc: 'Imaginify is a groundbreaking Software-as-a-Service application that empowers users to create stunning photo manipulations using AI technology. With features like AI-driven image editing, a payments system, and a credits-based model.',
-    subdesc:
-      'Built with Next.js 14, Cloudinary AI, Clerk, and Stripe, Imaginify combines cutting-edge technology with a user-centric approach. It can be turned into a side income or even a full-fledged business.',
-    href: 'https://www.youtube.com/watch?v=Ahwoks_dawU',
-    texture: '/textures/project/project5.mp4',
-    logo: '/assets/project-logo5.png',
-    logoStyle: {
-      backgroundColor: '#1C1A43',
-      border: '0.2px solid #252262',
-      boxShadow: '0px 0px 60px 0px #635BFF4D',
-    },
-    spotlight: '/assets/spotlight5.png',
-    tags: [
-      {
-        id: 1,
-        name: 'React.js',
-        path: '/assets/react.svg',
+        name: 'Kaggle',
+        path: 'assets/kaggle.png',
       },
       {
         id: 2,
-        name: 'TailwindCSS',
-        path: 'assets/tailwindcss.png',
-      },
+        name: 'Pandas',
+        path: 'assets/pandas.png',
+      }, 
       {
         id: 3,
-        name: 'TypeScript',
-        path: '/assets/typescript.png',
-      },
-      {
-        id: 4,
-        name: 'Framer Motion',
-        path: '/assets/framer.png',
-      },
+        name: 'Pandas',
+        path: 'assets/collab.png',
+      }
     ],
-  },
+  }
 ];
 
 export const calculateSizes = (isSmall, isMobile, isTablet) => {
