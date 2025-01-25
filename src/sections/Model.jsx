d7a1946f094064e6980221110138ab031ac6d1c4
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-25 13:58:22 +0530

    fix:href

--- src/sections/Model.jsx
+++ src/sections/Model.jsx
@@ -67,7 +67,7 @@ export default function Model() {
         </Canvas>
       </div>      
       <div className={`absolute h-${sizes.button} bottom-7 left-0 right-0 w-full z-10 c-space`}>
-        <a href="#contact" className='w-fit'>
+        <a className='w-fit'>
           <Button text="Let's Work together" containerClass="sm:w-fit w-full sm:min-w-96"/>
         </a>
       </div>
