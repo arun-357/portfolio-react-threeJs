042486600721c18c55f26c4cda49f3cd7f239f59
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-25 18:43:12 +0530

    add:envVar

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
+/* eslint-disable no-undef */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.VITE_SERVICE_ID':JSON.stringify(process.env.VITE_SERVICE_ID),
+    'process.env.VITE_TEMPLATE_ID':JSON.stringify(process.env.VITE_TEMPLATE_ID),
+    'process.env.VITE_TO_EMAIL':JSON.stringify(process.env.VITE_TO_EMAIL),
+    'process.env.VITE_EMAIL_KEY':JSON.stringify(process.env.VITE_EMAIL_KEY)
+  }
 })
