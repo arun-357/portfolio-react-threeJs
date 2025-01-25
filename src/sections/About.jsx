88e3da0cbeb801d81c813dcb83f7deb21a4f0cac
arun-357s <arunachalamnachiappan20@gmail.com>
2025-01-25 19:08:10 +0530

    fix:copyButton

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -108,7 +108,7 @@ const About = () => {
               <p className="grid-headtext">Communications & Locations</p>
               <p className="grid-subtext">I&apos;m based in Coimbatore, India and open to work Pan India.</p>
               <div className="copy-container p-4 mt-2" onClick={handleCopy}>
-                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" />
+                <img src={hasCopied ? 'assets/tick.svg' : 'assets/copy.svg'} alt="copy" className="w-7 h-7"/>
                 <p className="text-sm font-medium text-gray_gradient text-white">arunachalamnachiappan20@gamil.com</p>
               </div>
             </div>
